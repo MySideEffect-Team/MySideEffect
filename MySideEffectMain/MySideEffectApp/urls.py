b5e771bde838d12ed28fbf89466c77c03f432dff
Mxtreme1 <mr492@students.uni-freiburg.de>
2017-10-28 20:42:37 +0200

    added contact and sponsors page

--- MySideEffectMain/MySideEffectApp/urls.py
+++ MySideEffectMain/MySideEffectApp/urls.py
@@ -9,7 +9,9 @@ from . import views
 app_name = 'MySideEffectApp'
 urlpatterns = [
     url(r'^$', views.home, name='home'),
-    url(r'^about$', views.about, name='about')
+    url(r'^about$', views.about, name='about'),
+    url(r'^contact$', views.contact, name='contact'),
+    url(r'^sponsors$', views.sponsors, name='sponsors')
    # url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
    # url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
    # url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
