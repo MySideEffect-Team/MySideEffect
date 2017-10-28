96b389e38fc80cfb745443599568c858c93a4e2a
Mxtreme1 <mr492@students.uni-freiburg.de>
2017-10-28 15:30:40 +0200

    forms update

--- MySideEffectMain/MySideEffectApp/forms.py
+++ MySideEffectMain/MySideEffectApp/forms.py
@@ -28,19 +28,36 @@ SPORTS = [
     ('1', 'Once a week'),
     ('2', 'Twice a week'),
     ('3', 'Thrice a week'),
-    ('over', 'More than three times a week'),
-]
+    ('over', 'More than three times a week'), ]
+
+EATING = [
+    ('meat', 'Regular'),
+    ('veggie', 'Vegetarian'),
+    ('vegan', 'Vegan'), ]
 
 
 class UserForm(forms.Form):
+
+    # General
+
     username = forms.CharField(max_length=100)
-    email1 = forms.CharField(max_length=100)
-    email2 = forms.CharField(max_length=100)
-    password1 = forms.CharField(max_length=100)
-    password2 = forms.CharField(max_length=100)
+    email = forms.CharField(max_length=100)
+    # email2 = forms.CharField(max_length=100)
+    password = forms.CharField(max_length=100)
+    # password2 = forms.CharField(max_length=100)
     first_name = forms.CharField(max_length=100)
     last_name = forms.CharField(max_length=100)
     email = forms.EmailField()
     gender = forms.CharField(label='What is your Gender?', widget=forms.RadioSelect(choices=GENDER))
     age = forms.ChoiceField(label='How old are you?', choices=AGE, required=True)
     location = forms.ChoiceField(label='Where do you live?', required=True, choices=LOCATION)
+
+    # Lifestyle
+
+    sports = forms.ChoiceField(label='How often do you do sports?',
+                               required=True, choices=SPORTS)
+
+    eating = forms.ChoiceField(label='What are your eating habits?',
+                               required=True, choices=EATING)
+    smoking = forms.BooleanField(initial=True)
+    drinking = forms.BooleanField(label='Do you drink alcohol', initial=True)
