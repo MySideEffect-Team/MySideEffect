fec13e441852b9cfc7809417a689b322b4b4ccd8
Mxtreme1 <mr492@students.uni-freiburg.de>
2017-10-28 17:11:14 +0200

    new attributes, questions, beautify

--- MySideEffectMain/MySideEffectApp/forms.py
+++ MySideEffectMain/MySideEffectApp/forms.py
@@ -31,10 +31,11 @@ SPORTS = [
     ('3', 'Thrice a week'),
     ('over', 'More than three times a week'), ]
 
-EATING = [
-    ('meat', 'Regular'),
-    ('veggie', 'Vegetarian'),
-    ('vegan', 'Vegan'), ]
+MEAT = [
+    ('0', 'No'),
+    ('1', '1-2'),
+    ('2', '3'),
+    ('3', '>3'), ]
 
 CHILDREN = [
     ('none', 'No children'),
@@ -43,6 +44,14 @@ CHILDREN = [
     ('3', 'Three children'),
     ('more', 'Four or more children'),
 ]
+
+WEIGHT = [
+    ('35', '<35'),
+    ('50', '35-50'),
+    ('65', '51-65'),
+    ('80', '66-80'),
+    ('95', '81-95'),
+    ('100', 'Over 95'), ]
 
 
 class UserForm(forms.Form):
@@ -57,24 +66,27 @@ class UserForm(forms.Form):
     # first_name = forms.CharField(max_length=100)
     # last_name = forms.CharField(max_length=100)
     # email = forms.EmailField()
-    gender = forms.CharField(label='What is your Gender?', widget=forms.RadioSelect(choices=GENDER))
-    age = forms.ChoiceField(label='How old are you?', choices=AGE, required=True)
-    location = forms.ChoiceField(label='Where do you live?', required=True, choices=LOCATION)
+
+    gender = forms.CharField(label='What is your Gender', widget=forms.RadioSelect(choices=GENDER))
+    age = forms.ChoiceField(label='How old are you', choices=AGE, required=False)
+    weight = forms.ChoiceField(label='How much do you weigh', required=False,
+                               choices='WEIGHT')
+    location = forms.ChoiceField(label='Where do you live', required=False, choices=LOCATION)
 
     # Lifestyle
 
-    sports = forms.ChoiceField(label='How often do you do sports?',
-                               required=True, choices=SPORTS)
-
-    eating = forms.ChoiceField(label='What are your eating habits?',
-                               required=True, choices=EATING)
-    smoking = forms.BooleanField(label='Are you a smoker?', initial=False)
-    drinking = forms.BooleanField(label='Do you drink alcohol?', initial=False)
+    sports = forms.ChoiceField(label='How often do you do sports',
+                               required=False, choices=SPORTS)
+    veggie = forms.BooleanField(label='Do you eat meat', initial=True)
+    eating = forms.ChoiceField(label='How often do you eat meat',
+                               required=False, choices=MEAT, initial='0')
+    smoking = forms.BooleanField(label='Are you a smoker', initial=False)
+    drinking = forms.BooleanField(label='Do you drink alcohol', initial=False)
 
     # Medical history
 
-    pregnant = forms.BooleanField(label='Are you pregnant', initial=False)
     drugs = forms.CharField(label='List all drugs you currently take (single line for each)', widget=forms.Textarea)
     allergies = forms.CharField(label='List of allergies and chronic diseases (single line each)', widget=forms.Textarea)
     nodrugs = forms.CharField(label='Do not include the following drugs (single line each)', widget=forms.Textarea)
-    children = forms.ChoiceField(label='Do you have children', required=True, choices=CHILDREN)
+    pregnant = forms.BooleanField(label='Are you pregnant', initial=False)
+    children = forms.ChoiceField(label='Do you have children and if yes how many', required=False, choices=CHILDREN)
