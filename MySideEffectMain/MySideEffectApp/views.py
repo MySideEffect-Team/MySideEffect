142b0671bede6c22842a759e56776435a259cc8b
MFreidank <freidankm@yahoo.de>
2017-10-28 19:45:22 +0200

    updated view

--- MySideEffectMain/MySideEffectApp/views.py
+++ MySideEffectMain/MySideEffectApp/views.py
@@ -18,10 +18,23 @@ def home(request):
         if form.is_valid():
             form_weight = form.cleaned_data["weight"]
             form_age = form.cleaned_data["age"]
+
+            if "-" in form_age:
+                lower_age, upper_age = map(int, form_age.split("-"))
+            else:
+                if form_age.startswith("under"):
+                    lower_age = 0
+                    upper_age = int(form_age.lstrip("under"))
+                elif form_age.startswith("over"):
+                    lower_age = int(form_age.lstrip("under"))
+                    upper_age = 1000
+                else:
+                    raise ValueError("Invalid age specified!")
+
             form_gender = form.cleaned_data["gender"]
             form_location = form.cleaned_data["location"]
 
-            res_list = Occurence.objects.filter(age__lte=form_age).filter(age__gte=form_age)
+            res_list = Occurence.objects.filter(age__lte=upper_age).filter(age__gte=lower_age)
             return render(request, 'MySideEffectApp/result.html', {'res_list': res_list})
 
     personal_info_form = UserForm()
