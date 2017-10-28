c3d6adbe007284e96eeb76c44f59912150f3f5bd
Moritz Rocholl <moritz.rocholl@yahoo.de>
2017-10-28 13:45:13 +0200

    updated forms

--- MySideEffectMain/MySideEffectApp/forms.py
+++ MySideEffectMain/MySideEffectApp/forms.py
@@ -5,44 +5,44 @@ from django import forms
 
 # General information
 GENDER = [
-   ('female', 'Female'),
-   ('male', 'Male'), ]
+    ('female', 'Female'),
+    ('male', 'Male'), ]
 
 AGE = [
-   ('under', 'Under 25'),
-   ('35', '25-35'),
-   ('50', '36-50'),
-   ('65', '51-65'),
-   ('80', '66-80'),
-   ('over', 'Over 80'), ]
+    ('under', 'Under 25'),
+    ('35', '25-35'),
+    ('50', '36-50'),
+    ('65', '51-65'),
+    ('80', '66-80'),
+    ('over', 'Over 80'), ]
 
 LOCATION = [
-   ('asia', 'Asia'),
-   ('america', 'America'),
-   ('europe', 'Europe'),
-   ('africa', 'Africa'), ]
+    ('asia', 'Asia'),
+    ('america', 'America'),
+    ('europe', 'Europe'),
+    ('africa', 'Africa'), ]
 
 # Lifestlye information
 SPORTS = [
-   ('not', 'Not at all'),
-   ('1', 'Once a week'),
-   ('2', 'Twice a week'),
-   ('3', 'Thrice a week'),
-   ('over', 'More than three times a week'),
+    ('not', 'Not at all'),
+    ('1', 'Once a week'),
+    ('2', 'Twice a week'),
+    ('3', 'Thrice a week'),
+    ('over', 'More than three times a week'),
 ]
 
 
 class UserForm(forms.Form):
-   username = forms.CharField(max_length=100)
-   email1 = forms.CharField(max_length=100)
-   email2 = forms.CharField(max_length=100)
-   password1 = forms.CharField(max_length=100)
-   password2 = forms.CharField(max_length=100)
-   first_name = forms.CharField(max_length=100)
-   last_name = forms.CharField(max_length=100)
-   email = forms.EmailField()
-   gender = forms.CharField(label='What is your Gender?', widget=forms.RadioSelect(choices=GENDER))
-   age = forms.CharField(label='How old are you?',
-                         widget=forms.ChoiceField(choices=AGE, required=True))
+    username = forms.CharField(max_length=100)
+    email1 = forms.CharField(max_length=100)
+    email2 = forms.CharField(max_length=100)
+    password1 = forms.CharField(max_length=100)
+    password2 = forms.CharField(max_length=100)
+    first_name = forms.CharField(max_length=100)
+    last_name = forms.CharField(max_length=100)
+    email = forms.EmailField()
+    gender = forms.CharField(label='What is your Gender?', widget=forms.RadioSelect(choices=GENDER))
+    age = forms.CharField(label='How old are you?',
+                          widget=forms.ChoiceField(choices=AGE, required=True))
    location = forms.CharField(label='Where do you live?',
                               widget=forms.ChoiceField(choices=LOCATION, required=True))
